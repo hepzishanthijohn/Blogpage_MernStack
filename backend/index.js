b79bdfdbbaba774db1b471fd096a306ddd59a3cd
hepzishanthijohn <yyepsipal@gmail.com>
2024-08-13 17:54:30 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 const cors = require('cors');
-app.use(cors({
-  credentials: true
-}));
+app.use(cors());
 app.use(cookieParser())
 //the route 
 app.use("/api/users" , uploadRoute);
