34308f7ebc89c176f37fac4ab56bcf8cec29433e
hepzishanthijohn <yyepsipal@gmail.com>
2024-08-13 18:00:15 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'https://blogpage-mern-stack.vercel.app'
+}));
 app.use(cookieParser())
 //the route 
 app.use("/api/users" , uploadRoute);
